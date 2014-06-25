6d4469b7ed0edf201d41ca32216623b0b4224a70
ulrik nielsen <ulrik@o3.dk>
2014-06-25 21:29:44 +0200

    missed a _ in the configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('phpredis');
+        $rootNode = $treeBuilder->root('php_redis');
 
         $rootNode
             ->children()
